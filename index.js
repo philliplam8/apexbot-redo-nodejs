8fae52a70179d60f7bd1b744a2b21c0f523c45bc
Phillip Lam <phillip.lam0@gmail.com>
2022-01-29 21:19:38 -0500

    fix: added ESLint and corrected spacing/tabs and replaced all var with const/let

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var express = require('express'); // Express web server framework
-var cors = require('cors');
-var cookieParser = require('cookie-parser');
-var request = require('request'); // "Request" library
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const request = require('request');
 require('dotenv').config();
 const { Client, Intents, MessageEmbed, MessageFlags } = require('discord.js');
 const { bold, italic, strikethrough, underscore, spoiler, quote, blockQuote } = require('@discordjs/builders');
+const GIBBY_LAUGH = 'https://lh3.googleusercontent.com/pw/AM-JKLVGx1ZWfcDVTgCVCEAZ2ks1e-grT6oO2rEZ4LWWK5B6ZTLwV0wl3iCg9Nx068KfLrncH3aL2q5rxkshX913QMc0zeRd16g-VJpljzI8amJbpPwnICrSCSchC9QKMQott6UaHXJGVnaUbttOWC6pRMnq2Q=w1022-h466-no?authuser=0';
 
 // Environment Variables
 const PORT = process.env.PORT || 5000;
@@ -13,21 +14,21 @@ const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 // APEX API URL
 const APEX_API_HOST = 'https://api.mozambiquehe.re';
-const APEX_API_PATH = '/maprotation?version=2'
+const APEX_API_PATH = '/maprotation?version=2';
 const GIBBY_IMG = 'https://images.gnwcdn.com/2020/usgamer/apex-legends-gibby-bug.jpg';
 
 // Regex
 const pattern = /sad/i;
 
-var app = express();
+const app = express();
 
 app.use(express.static(__dirname + '/public'))
-    .use(cors())
-    .use(cookieParser());
+	.use(cors())
+	.use(cookieParser());
 
 // Apex API GET Requests ------------------------------------------------------
-var mapData = '';
-var mapMessage = 'not started';
+let mapData = '';
+let mapMessage = 'not started';
 
 
 // Setting the configuration for the request
@@ -35,95 +36,95 @@ const url = 'https://api.mozambiquehe.re/maprotation?version=2&auth=' + APEX_API
 
 function getCurrentMap() {
 
-    request.get(url, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            mapData = JSON.parse(body);
+	request.get(url, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			mapData = JSON.parse(body);
 
-            // Battle Royale Data
-            var brData = mapData.battle_royale;
-            var brCurrentMap = brData.current.map;
-            var brCurrentMapRemainingMinutes = brData.current.remainingMins;
-            var brCurrentMapRemainingTimer = brData.current.remainingTimer;
-            var brCurrentMapImage = brData.current.asset;
-            var brNextMap = brData.next.map;
+			// Battle Royale Data
+			const brData = mapData.battle_royale;
+			const brCurrentMap = brData.current.map;
+			const brCurrentMapRemainingMinutes = brData.current.remainingMins;
+			const brCurrentMapRemainingTimer = brData.current.remainingTimer;
+			const brCurrentMapImage = brData.current.asset;
+			const brNextMap = brData.next.map;
 
-            // Arenas Data
-            var arenaData = mapData.arenas;
-            var arenasCurrentMap = arenaData.current.map;
-            var arenasCurrentMapRemainingMinutes = arenaData.current.remainingMins;
-            var arenasCurrentMapRemainingTimer = arenaData.current.remainingTimer;
-            var areansCurrentMapImage = arenaData.current.asset;
-            var arenasNextMap = arenaData.next.map;
+			// Arenas Data
+			const arenaData = mapData.arenas;
+			const arenasCurrentMap = arenaData.current.map;
+			const arenasCurrentMapRemainingMinutes = arenaData.current.remainingMins;
+			const arenasCurrentMapRemainingTimer = arenaData.current.remainingTimer;
+			const areansCurrentMapImage = arenaData.current.asset;
+			const arenasNextMap = arenaData.next.map;
 
-            // Construct Message
-            var battleRoyaleMessage = "**Battle Royale:**\nThe current map is **" + brCurrentMap + "** for the next **" + brCurrentMapRemainingMinutes + "** minutes.\nThe next map is **" + brNextMap + "** bruddas."
-            var arenasMessage = "\n\n**Arenas:**\nThe current map is **" + arenasCurrentMap + "** for the next **" + arenasCurrentMapRemainingMinutes + "** minutes.\nThe next map is **" + arenasNextMap + "**."
-            mapMessage = battleRoyaleMessage + arenasMessage;
+			// Construct Message
+			const battleRoyaleMessage = '**Battle Royale:**\nThe current map is **' + brCurrentMap + '** for the next **' + brCurrentMapRemainingMinutes + '** minutes.\nThe next map is **' + brNextMap + '** bruddas.';
+			const arenasMessage = '\n\n**Arenas:**\nThe current map is **' + arenasCurrentMap + '** for the next **' + arenasCurrentMapRemainingMinutes + '** minutes.\nThe next map is **' + arenasNextMap + '**.';
+			mapMessage = battleRoyaleMessage + arenasMessage;
 
-            var embedBattleRoyaleMessage = {
-                color: 0x0099ff,
-                title: 'Battle Royale',
-                fields: [
-                    {
-                        name: 'Current Map',
-                        value: brCurrentMap,
-                        inline: true,
-                    },
-                    {
-                        name: 'Remaining time',
-                        value: brCurrentMapRemainingTimer,
-                        inline: true,
-                    },
-                ],
-                image: {
-                    url: brCurrentMapImage
-                },
-                footer: {
-                    text: 'Next map: ' + brNextMap,
-                }
-            }
+			const embedBattleRoyaleMessage = {
+				color: 0x0099ff,
+				title: 'Battle Royale',
+				fields: [
+					{
+						name: 'Current Map',
+						value: brCurrentMap,
+						inline: true,
+					},
+					{
+						name: 'Remaining time',
+						value: brCurrentMapRemainingTimer,
+						inline: true,
+					},
+				],
+				image: {
+					url: brCurrentMapImage,
+				},
+				footer: {
+					text: 'Next map: ' + brNextMap,
+				},
+			};
 
-            var embedArenasMessage = {
-                color: 0x0099ff,
-                title: 'Arenas',
-                fields: [
-                    {
-                        name: 'Current Map',
-                        value: arenasCurrentMap,
-                        inline: true,
-                    },
-                    {
-                        name: 'Remaining time',
-                        value: arenasCurrentMapRemainingTimer,
-                        inline: true,
-                    },
-                ],
-                image: {
-                    url: areansCurrentMapImage
-                },
-                footer: {
-                    text: 'Next map: ' + arenasNextMap,
-                }
-            }
+			const embedArenasMessage = {
+				color: 0x0099ff,
+				title: 'Arenas',
+				fields: [
+					{
+						name: 'Current Map',
+						value: arenasCurrentMap,
+						inline: true,
+					},
+					{
+						name: 'Remaining time',
+						value: arenasCurrentMapRemainingTimer,
+						inline: true,
+					},
+				],
+				image: {
+					url: areansCurrentMapImage,
+				},
+				footer: {
+					text: 'Next map: ' + arenasNextMap,
+				},
+			};
 
-            embedDemo = new MessageEmbed()
-                .setColor('0x0099ff')
-                .setTitle('Battle Royale');
+			embedDemo = new MessageEmbed()
+				.setColor('0x0099ff')
+				.setTitle('Battle Royale');
 
-            return;
-        }
-    });
+			return;
+		}
+	});
 }
 
 app.get('', async (req, res) => {
-    res.send("I'm up!");
-})
+	res.send('I\'m up!');
+});
 
 app.get('/apex', async (_, res) => {
-    getCurrentMap();
-    res.send(mapData);
-    res.end();
-})
+	getCurrentMap();
+	res.send(mapData);
+	res.end();
+});
 
 
 // // Discord Client -------------------------------------------------------------
@@ -132,118 +133,117 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 client.login(DISCORD_TOKEN);
 
 // client is an instance of Discord.Client
-client.on("messageCreate", async (message) => {
+client.on('messageCreate', async (message) => {
 
-    getCurrentMap();
+	getCurrentMap();
 
-    if (message.content == "!ping") { // Check if content of message is "!ping"
-        message.channel.send("pong!"); // Call .send() on the channel object the message was sent in
-    }
+	if (message.content == '!ping') { // Check if content of message is "!ping"
+		message.channel.send('pong!'); // Call .send() on the channel object the message was sent in
+	}
 
-    if (message.content == "!test") {
-        request.get(url, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
+	if (message.content == '!test') {
+		request.get(url, function(error, response, body) {
+			if (!error && response.statusCode === 200) {
 
-                getCurrentMap();
+				getCurrentMap();
 
-                // TODO: add a promise here to wait until getCurrentMap is updated before message is sent
-                message.channel.send(mapMessage);
-            }
-        })
-    }
+				// TODO: add a promise here to wait until getCurrentMap is updated before message is sent
+				message.channel.send(mapMessage);
+			}
+		});
+	}
 
-    if (message.content == "!map") {
-        request.get(url, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                mapData = JSON.parse(body);
+	if (message.content == '!map') {
+		request.get(url, function(error, response, body) {
+			if (!error && response.statusCode === 200) {
+				mapData = JSON.parse(body);
 
-                // Battle Royale Data
-                var brData = mapData.battle_royale;
-                var brCurrentMap = brData.current.map;
-                var brCurrentMapRemainingMinutes = brData.current.remainingMins;
-                var brCurrentMapRemainingTimer = brData.current.remainingTimer;
-                var brCurrentMapImage = brData.current.asset;
-                var brNextMap = brData.next.map;
+				// Battle Royale Data
+				const brData = mapData.battle_royale;
+				const brCurrentMap = brData.current.map;
+				const brCurrentMapRemainingMinutes = brData.current.remainingMins;
+				const brCurrentMapRemainingTimer = brData.current.remainingTimer;
+				const brCurrentMapImage = brData.current.asset;
+				const brNextMap = brData.next.map;
 
-                // Arenas Data
-                var arenaData = mapData.arenas;
-                var arenasCurrentMap = arenaData.current.map;
-                var arenasCurrentMapRemainingMinutes = arenaData.current.remainingMins;
-                var arenasCurrentMapRemainingTimer = arenaData.current.remainingTimer;
-                var areansCurrentMapImage = arenaData.current.asset;
-                var arenasNextMap = arenaData.next.map;
+				// Arenas Data
+				const arenaData = mapData.arenas;
+				const arenasCurrentMap = arenaData.current.map;
+				const arenasCurrentMapRemainingMinutes = arenaData.current.remainingMins;
+				const arenasCurrentMapRemainingTimer = arenaData.current.remainingTimer;
+				const areansCurrentMapImage = arenaData.current.asset;
+				const arenasNextMap = arenaData.next.map;
 
-                // Construct Message
-                var battleRoyaleMessage = "**Battle Royale:**\nThe current map is **" + brCurrentMap + "** for the next **" + brCurrentMapRemainingMinutes + "** minutes.\nThe next map is **" + brNextMap + "** bruddas."
-                var arenasMessage = "\n\n**Arenas:**\nThe current map is **" + arenasCurrentMap + "** for the next **" + arenasCurrentMapRemainingMinutes + "** minutes.\nThe next map is **" + arenasNextMap + "**."
-                mapMessage = battleRoyaleMessage + arenasMessage;
+				// Construct Message
+				const battleRoyaleMessage = '**Battle Royale:**\nThe current map is **' + brCurrentMap + '** for the next **' + brCurrentMapRemainingMinutes + '** minutes.\nThe next map is **' + brNextMap + '** bruddas.';
+				const arenasMessage = '\n\n**Arenas:**\nThe current map is **' + arenasCurrentMap + '** for the next **' + arenasCurrentMapRemainingMinutes + '** minutes.\nThe next map is **' + arenasNextMap + '**.';
+				mapMessage = battleRoyaleMessage + arenasMessage;
 
-                var embedBattleRoyaleMessage = {
-                    color: 0x0099ff,
-                    title: 'Battle Royale',
-                    fields: [
-                        {
-                            name: 'Current Map',
-                            value: brCurrentMap,
-                            inline: true,
-                        },
-                        {
-                            name: 'Remaining Time',
-                            value: brCurrentMapRemainingTimer,
-                            inline: true,
-                        },
-                    ],
-                    image: {
-                        url: brCurrentMapImage
-                    },
-                    footer: {
-                        text: 'Next map: ' + brNextMap,
-                    }
-                }
+				const embedBattleRoyaleMessage = {
+					color: 0x0099ff,
+					title: 'Battle Royale',
+					thumbnail: {
+						url: GIBBY_LAUGH,
+					},
+					fields: [
+						{
+							name: 'Current Map',
+							value: brCurrentMap,
+							inline: true,
+						},
+						{
+							name: 'Remaining Time',
+							value: brCurrentMapRemainingTimer,
+							inline: true,
+						},
+					],
+					image: {
+						url: brCurrentMapImage,
+					},
+					footer: {
+						text: 'Next map: ' + brNextMap,
+					},
+				};
 
-                var embedArenasMessage = {
-                    color: 0xC86A6F,
-                    title: 'Arenas',
-                    fields: [
-                        {
-                            name: 'Current Map',
-                            value: arenasCurrentMap,
-                            inline: true,
-                        },
-                        {
-                            name: 'Remaining Time',
-                            value: arenasCurrentMapRemainingTimer,
-                            inline: true,
-                        },
-                    ],
-                    image: {
-                        url: areansCurrentMapImage
-                    },
-                    footer: {
-                        text: 'Next map: ' + arenasNextMap,
-                    }
-                }
+				const embedArenasMessage = {
+					color: 0xC86A6F,
+					title: 'Arenas',
+					fields: [
+						{
+							name: 'Current Map',
+							value: arenasCurrentMap,
+							inline: true,
+						},
+						{
+							name: 'Remaining Time',
+							value: arenasCurrentMapRemainingTimer,
+							inline: true,
+						},
+					],
+					image: {
+						url: areansCurrentMapImage,
+					},
+					footer: {
+						text: 'Next map: ' + arenasNextMap,
+					},
+				};
 
-                embedDemo = new MessageEmbed()
-                    .setColor('0x0099ff')
-                    .setTitle('Battle Royale');
+				message.channel.send({ embeds: [embedBattleRoyaleMessage, embedArenasMessage] });
+			}
+		});
 
-                message.channel.send({ embeds: [embedBattleRoyaleMessage, embedArenasMessage] });
-            }
-        });
+	}
 
-    }
-
-    // Using regex, check all cases for text "sad" 
-    if (pattern.test(message.content)) {
-        var gibbyJSON = require("./gibby_quotes.json");
-        var RNG = (Math.floor(Math.random() * 10)).toString();
-        var wholesomeMessage = blockQuote(gibbyJSON.quotes[RNG].quote);
-        message.reply(wholesomeMessage);
-    }
+	// Using regex, check all cases for text "sad"
+	if (pattern.test(message.content)) {
+		const gibbyJSON = require('./gibby_quotes.json');
+		const RNG = (Math.floor(Math.random() * 10)).toString();
+		const wholesomeMessage = blockQuote(gibbyJSON.quotes[RNG].quote);
+		message.reply(wholesomeMessage);
+	}
 });
 
 // Create a server and listen to it.
 app.listen(PORT, () => {
-    console.log(`Application is live and listening on port ${PORT}`);
+	console.log(`Application is live and listening on port ${PORT}`);
 });
