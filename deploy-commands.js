f061d63cfecccf450ba153929ca9475a22fcb8bb
Phillip Lam <phillip.lam0@gmail.com>
2022-01-30 16:25:32 -0500

    feat: registered new slash commands for Apex bot

---
+++ deploy-commands.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+require('dotenv').config();
+
+// Environment Variables
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN_DEV;
+const CLIENT_ID = process.env.CLIENT_ID_DEV;
+const GUILD_ID = process.env.GUILD_ID_DEV;
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('map').setDescription('Replies with APEX map rotation info, bruddah'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
