f5c88dbc767750899666b9cc174ad08ec414c53a
Phillip <phillip.lam0@gmail.com>
2022-04-17 23:42:39 -0400

    refactor: abstracted Apex API request logic into services module

--- index.js
+++ index.js
@@ -1,68 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const request = require('request');
 const { Client, Intents } = require('discord.js');
-const GIBBY_LAUGH = 'https://lh3.googleusercontent.com/pw/AM-JKLVGx1ZWfcDVTgCVCEAZ2ks1e-grT6oO2rEZ4LWWK5B6ZTLwV0wl3iCg9Nx068KfLrncH3aL2q5rxkshX913QMc0zeRd16g-VJpljzI8amJbpPwnICrSCSchC9QKMQott6UaHXJGVnaUbttOWC6pRMnq2Q=w1022-h466-no?authuser=0';
 const helpers = require('./utils/helpers.util.js');
 require('dotenv').config();
 const config = require('./configs/general.config.js');
+const mapRotation = require('./services/map-rotation.service');
 
 const app = express();
 
 app.use(express.static(__dirname + '/public'))
 	.use(cors())
 	.use(cookieParser());
-
-// Apex API GET Requests ------------------------------------------------------
-
-// Setting the configuration for the request
-const url = config.APEX_API_URL + config.APEX_API_TOKEN;
-
-async function sendChannelMessage(hook, message, isSlashCommand) {
-
-	if (isSlashCommand) {
-		hook.reply(message);
-	}
-	else {
-		hook.channel.send(message);
-	}
-}
-
-async function getCurrentMap(hook, isSlashCommand) {
-
-	request.get(url, async function (error, response, body) {
-		if (!error && response.statusCode === 200) {
-			const mapData = JSON.parse(body);
-
-			// Battle Royale Data
-			const brData = mapData.battle_royale;
-			const brCurrentMap = brData.current.map;
-			// const brCurrentMapRemainingMinutes = brData.current.remainingMins;
-			const brCurrentMapRemainingTimer = brData.current.remainingTimer;
-			const brCurrentMapImage = brData.current.asset;
-			const brNextMap = brData.next.map;
-
-			// Arenas Data
-			const arenaData = mapData.arenas;
-			const arenasCurrentMap = arenaData.current.map;
-			// const arenasCurrentMapRemainingMinutes = arenaData.current.remainingMins;
-			const arenasCurrentMapRemainingTimer = arenaData.current.remainingTimer;
-			const areansCurrentMapImage = arenaData.current.asset;
-			const arenasNextMap = arenaData.next.map;
-
-			// Construct Messages
-			const embedBattleRoyaleMessage = helpers.createEmbeddedMessage('Battle Royale', GIBBY_LAUGH, brCurrentMap,
-				brCurrentMapRemainingTimer, brCurrentMapImage, brNextMap);
-			const embedArenasMessage = helpers.createEmbeddedMessage('Arenas', GIBBY_LAUGH, arenasCurrentMap,
-				arenasCurrentMapRemainingTimer, areansCurrentMapImage, arenasNextMap);
-			const message = { embeds: [embedBattleRoyaleMessage, embedArenasMessage] };
-
-			// Send messages
-			await sendChannelMessage(hook, message, isSlashCommand);
-		}
-	});
-}
 
 app.get('', async (req, res) => {
 	res.send('I\'m up!');
@@ -96,7 +45,7 @@ client.on('messageCreate', async (message) => {
 
 	// If content of message is "!map", return the map rotation
 	if (message.content == '!map') {
-		await getCurrentMap(message, false);
+		await mapRotation.getCurrentMap(message, false);
 	}
 });
 
@@ -116,7 +65,7 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	}
 	else if (commandName === 'map') {
-		await getCurrentMap(interaction, true);
+		await mapRotation.getCurrentMap(interaction, true);
 	}
 });
 
---
+++ services/map-rotation.service.js
@@ -0,0 +1,59 @@
+const helpers = require('../utils/helpers.util.js');
+const request = require('request');
+require('dotenv').config({ path: '../.env' });
+const config = require('../configs/general.config.js');
+const GIBBY_LAUGH = 'https://lh3.googleusercontent.com/pw/AM-JKLVGx1ZWfcDVTgCVCEAZ2ks1e-grT6oO2rEZ4LWWK5B6ZTLwV0wl3iCg9Nx068KfLrncH3aL2q5rxkshX913QMc0zeRd16g-VJpljzI8amJbpPwnICrSCSchC9QKMQott6UaHXJGVnaUbttOWC6pRMnq2Q=w1022-h466-no?authuser=0';
+
+const url = config.APEX_API_URL + config.APEX_API_TOKEN;
+
+async function sendChannelMessage(hook, message, isSlashCommand) {
+
+	if (isSlashCommand) {
+		await hook.reply(message);
+	}
+	else {
+		await hook.channel.send(message);
+	}
+}
+
+async function getCurrentMap(hook, isSlashCommand) {
+
+	// console.log(config, url);
+
+	request.get(url, async function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			const mapData = JSON.parse(body);
+
+			// Battle Royale Data
+			const brData = mapData.battle_royale;
+			const brCurrentMap = brData.current.map;
+			// const brCurrentMapRemainingMinutes = brData.current.remainingMins;
+			const brCurrentMapRemainingTimer = brData.current.remainingTimer;
+			const brCurrentMapImage = brData.current.asset;
+			const brNextMap = brData.next.map;
+
+			// Arenas Data
+			const arenaData = mapData.arenas;
+			const arenasCurrentMap = arenaData.current.map;
+			// const arenasCurrentMapRemainingMinutes = arenaData.current.remainingMins;
+			const arenasCurrentMapRemainingTimer = arenaData.current.remainingTimer;
+			const areansCurrentMapImage = arenaData.current.asset;
+			const arenasNextMap = arenaData.next.map;
+
+			// Construct Messages
+			const embedBattleRoyaleMessage = helpers.createEmbeddedMessage('Battle Royale', GIBBY_LAUGH, brCurrentMap,
+				brCurrentMapRemainingTimer, brCurrentMapImage, brNextMap);
+			const embedArenasMessage = helpers.createEmbeddedMessage('Arenas', GIBBY_LAUGH, arenasCurrentMap,
+				arenasCurrentMapRemainingTimer, areansCurrentMapImage, arenasNextMap);
+			const message = { embeds: [embedBattleRoyaleMessage, embedArenasMessage] };
+
+			// Send messages
+			await sendChannelMessage(hook, message, isSlashCommand);
+		}
+	});
+}
+
+module.exports = {
+	getCurrentMap,
+	sendChannelMessage,
+};
